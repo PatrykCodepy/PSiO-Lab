63a8fc346be833f3c0c64e4fc92cc6cce41d1d81
PatrykCodePy <patryk65249@gmail.com>
2021-11-21 22:06:40 +0100

    Create 2.cpp

---
+++ Zadanie_2/2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<size_t> find_all(std::string input, char a) {
+	size_t position = 0;
+	std::vector<size_t> New = {};
+	while ((position = input.find(a, position + 1)) != std::string::npos) {
+		New.push_back(position);
+	}
+	return New;
+}
+
+int main() {
+	std::string input = "Ala ma kota";
+
+	std::vector<size_t> pos = find_all(input, 'a'); // wynik: {2, 5, 10}
+	for (auto i : pos) std::cout << i << '\n';
+}
